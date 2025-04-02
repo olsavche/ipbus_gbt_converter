6c62b3ea1ba3d3fd453603f1d8acb8050642ab06
149.156.121.207 <not_use>
2025-04-02 21:22:45 +0200

    change mux

--- sim/mux.vhd
+++ sim/mux.vhd
@@ -5,7 +5,7 @@ use work.m_package.all;
 use work.tb_package.all;
 
 -- sel 0  -> ipbus 
--- sel 1  -> pbus <-> gbt
+-- sel 1  -> ipbus <-> gbt
 
 entity mux is
     generic (
@@ -13,14 +13,14 @@ entity mux is
     );
     port (
         -- ipbus tcm
-            i_wbus_tcm      : in ipb_wbus;
-            o_rbus_tcm      : out ipb_rbus;
+            i_wbus_kc705      : in ipb_wbus;
+            o_rbus_kc705      : out ipb_rbus;
         -- payload 
-            o_wbus_pld      : out ipb_wbus;
-            i_rbus_pld      : in ipb_rbus;
+            o_wbus_mem      : out ipb_wbus;
+            i_rbus_mem      : in ipb_rbus;
         -- ipbus <-> gbt 
-            i_wbus_wr       : in ipb_wbus;
-            o_rbus_wr       : out ipb_rbus;
+            i_wbus_converter       : in ipb_wbus;
+            o_rbus_converter       : out ipb_rbus;
         -- clk and sel 
             i_reset         : in std_logic;
             i_ipb_clk       : in std_logic;
@@ -38,33 +38,33 @@ begin
         begin
             if rising_edge(i_ipb_clk) then
                 if i_reset = '1' then
-                    o_wbus_pld  <= ipb_wbus_zero;
-                    o_rbus_tcm  <= ipb_rbus_zero;
-                    o_rbus_wr   <= ipb_rbus_zero;
+                    o_wbus_mem  <= ipb_wbus_zero;
+                    o_rbus_kc705  <= ipb_rbus_zero;
+                    o_rbus_converter   <= ipb_rbus_zero;
                 elsif i_sel = '0' then
-                    o_wbus_pld  <= i_wbus_tcm;
-                    o_rbus_tcm  <= i_rbus_pld;
-                    o_rbus_wr   <= ipb_rbus_zero;
+                    o_wbus_mem  <= i_wbus_kc705;
+                    o_rbus_kc705  <= i_rbus_mem;
+                    o_rbus_converter   <= ipb_rbus_zero;
                 else
-                    o_wbus_pld  <= i_wbus_wr;
-                    o_rbus_wr   <= i_rbus_pld;
-                    o_rbus_tcm  <= ipb_rbus_zero;
+                    o_wbus_mem  <= i_wbus_converter;
+                    o_rbus_converter   <= i_rbus_mem;
+                    o_rbus_kc705  <= ipb_rbus_zero;
                 end if;
             end if;
         end process;
     end generate;
 
     gen_async : if not USE_CLK generate
-        process(i_sel, i_wbus_tcm, i_rbus_pld, i_wbus_wr)
+        process(all)--(i_sel, i_wbus_kc705, i_rbus_mem, i_wbus_converter)
         begin
                 if i_sel = '0' then
-                    o_wbus_pld  <= i_wbus_tcm;
-                    o_rbus_tcm  <= i_rbus_pld;
-                    o_rbus_wr   <= ipb_rbus_zero;
+                    o_wbus_mem  <= i_wbus_kc705;
+                    o_rbus_kc705  <= i_rbus_mem;
+                    o_rbus_converter   <= ipb_rbus_zero;
                 else
-                    o_wbus_pld  <= i_wbus_wr;
-                    o_rbus_wr   <= i_rbus_pld;
-                    o_rbus_tcm  <= ipb_rbus_zero;
+                    o_wbus_mem  <= i_wbus_converter;
+                    o_rbus_converter   <= i_rbus_mem;
+                    o_rbus_kc705  <= ipb_rbus_zero;
                 end if;
         end process;
     end generate;
